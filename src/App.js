8cd251653fc6d139b1d59949e917d683c68bacf9
Adeagbo Jadesola Rubies <jadesolaadeagbo@gmail.com>
2022-10-27 11:31:37 +0100

    Corrected structure and divs alongside, margins and paddings

--- src/App.js
+++ src/App.js
@@ -9,30 +9,40 @@ import IngressiveImg from "./Components/ingressive.svg"
 function App() {
   return (
     <div className="App">
-      <Profile/>
+      <div className="overall-top">
+        <div className="section">
+        <Profile/>
 
-      <div className="links">
-        <Link id="twitter-link" link="twitter.com/AdeagboJadesola" field="Twitter Link" />
-        <Link id="btn__zuri" link="https://training.zuri.team/" field="Zuri Team"/>
-        <Link id="books" link="https://books.zuri.team/" field="Zuri Books"/>
-        <Link id="book__python" link="https://books.zuri.team/" field="Python Books"/>
-        <Link id="pitch" link="https://background.zuri.team//python-for-beginners?ref_id=Rubies" field="Background check for coders"/>
-        <Link id="book__design" link="https://books.zuri.team/design-rules"field="Design Books"/>
+        <div className="links">
+          <Link id="twitter-link" link="twitter.com/AdeagboJadesola" field="Twitter Link" />
+          <Link id="btn__zuri" link="https://training.zuri.team/" field="Zuri Team"/>
+          <Link id="books" link="https://books.zuri.team/" field="Zuri Books"/>
+          <Link id="book__python" link="https://books.zuri.team/" field="Python Books"/>
+          <Link id="pitch" link="https://background.zuri.team//python-for-beginners?ref_id=Rubies" field="Background check for coders"/>
+          <Link id="book__design" link="https://books.zuri.team/design-rules"field="Design Books"/>
+        </div>
+        </div>
+      </div>
+      <div className="footer-one-container">
+        <div className='first-footer'>
+          <div className="sub">
+            <img src={slackImg} alt="Slack Image" />
+            <img src={githubImg} alt="" />
+          </div>
+        </div>
       </div>
 
-      <div className='first-footer'>
-        <img src={slackImg} alt="Slack Image" />
-        <img src={githubImg} alt="" />
-      </div>
-
-      <div className="second-footer">
+      <div className="footer-two-container">
+        <div className="second-footer">
         <div className="sub-footer">
         <img src={ZuriImg} alt="" />
         <p>HNG Internship 9 Front end Task</p>
         <img src={IngressiveImg} alt="" />
 
-      </div>
         </div>
+      </div>
+      
+      </div>
     </div>
   );
 }
