80451e46b9b953f6dd247087e31a69da05aa94d7
Adeagbo Jadesola Rubies <jadesolaadeagbo@gmail.com>
2022-10-27 10:30:19 +0100

    First footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Profile from "./Components/Profile"
 import Link from "./Components/Link" 
+import slackImg from "./Components/slack.svg"
+import githubImg from "./Components/github.svg"
+// import ZuriImg from "./Components/zuri.svg"
 
 
 function App() {
@@ -13,9 +16,20 @@ function App() {
         <Link id="btn__zuri" link="https://training.zuri.team/" field="Zuri Team"/>
         <Link id="books" link="https://books.zuri.team/" field="Zuri Books"/>
         <Link id="book__python" link="https://books.zuri.team/" field="Python Books"/>
-        <Link id="pitch" link="https://background.zuri.team/" field="Background check for coders"/>
+        <Link id="pitch" link="https://background.zuri.team//python-for-beginners?ref_id=Rubies" field="Background check for coders"/>
         <Link id="book__design" link="https://books.zuri.team/design-rules"field="Design Books"/>
       </div>
+
+      <div className='first-footer'>
+        <img src={slackImg} alt="Slack Image" />
+        <img src={githubImg} alt="" />
+      </div>
+
+      {/* <div className="second-footer">
+
+
+      </div> */}
+
     </div>
   );
 }
