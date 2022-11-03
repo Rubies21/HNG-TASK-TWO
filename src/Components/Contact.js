7fb754adf2a05837225b53da2e3b2d61891fb456
Adeagbo Jadesola Rubies <jadesolaadeagbo@gmail.com>
2022-11-03 18:42:02 +0100

    feat: Implemented form feature in Contact Component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,22 +11,36 @@ const Contact = () => {
 
         <form>
           <div className="names">
-          <div className="fname">
-            <label>
-            First Name
-            <input type="text" name='firstName' id='first_name' placeholder='First Name'/>
-            </label>
+
+            <div className="fields">
+              <label>
+              First Name</label>
+              <input type="text" name='firstName' id='first_name' placeholder='Enter your first name'/>
+            </div>
+    
+            <div className="fields">
+              <label>
+              Last Name </label>
+              <input type="text" name='lastName' id='last_name' placeholder='Enter your last name' />
             </div>
 
+          </div>
 
-            <div className="lname">
-            <label>
-            Last Name
-            <input type="text" name='lastName' id='last_name' placeholder='Last Name' />
-            </label>
-          </div>
-          </div>
           
+          <div className="email">
+              <label>Email</label>
+              <input type="email" name='email' id='email' placeholder='yourname@email.com' />
+            </div>
+
+          <div className="textArea">
+            <label>Message</label>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+          </div>
+
+          <div className="checkbox">
+            <input type="checkbox" id="checkbox"/>
+            <label id="checkbox_text">You agree to providing your data to Jadesola who may contact you</label>
+          </div>
          </form>
         </div>
 
